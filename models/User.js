a0b9218428dcf2814dd8fb9a824805571c36dbe0
mohamed <mohamedhussein6500@gmail.com>
2026-09-03 17:11:42 +0300

    more updates

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
-
+const jwt = require('jsonwebtoken')
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -30,4 +30,14 @@ UserSchema.pre('save',async function(){
   this.password = await bcrypt.hash(this.password,salt)
   
 })
+
+UserSchema.methods.createJWT = function(){
+  return jwt.sign({userId: this._id, name:this.name},process.env.JWT_SECRET,{
+    expiresIn:process.env.JWT_LIFETIME,
+  })
+}
+
+
+
+
 module.exports = mongoose.model('User',UserSchema)
