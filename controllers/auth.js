2e4674ddafada51e123db558d438105da289049e
mohamed <mohamedhussein6500@gmail.com>
2026-09-03 12:05:55 +0300

    implemented secuirty

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,17 @@
 const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes') 
+
 const register = async (req, res) => {
-    res.json(req.body)
+   
+    const user = await User.create({...req.body})
+    res.status(StatusCodes.CREATED).json({user})  
 }
 
 const login = async (req, res) => {
     res.send('login user')
 }
 
-module.exports = {  // ✅ Correct! (with 's')
+module.exports = {
     register,
     login,
 }
