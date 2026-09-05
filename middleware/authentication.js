2566eee9705f782cb4846d4f8a7b32bec4c328c2
mohamed <mohamedhussein6500@gmail.com>
2026-09-05 21:12:24 +0300

    implemented auth middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,24 +1,23 @@
 const User = require('../models/User')
 const jwt = require('jsonwebtoken')
-const {  UnauthenticatedError} = require('../errors/unauthenticated')
+const { UnauthenticatedError } = require('../errors/unauthenticated')
 
-const auth = (req,res,next)=>{
-    const authHeader = req.headers.authorization
-    if(!authHeader || !authHeader.startsWith('Bearer')){
-        throw new UnauthenticatedError('Authorization invalid')
-    }
-}
+const auth = (req, res, next) => {
+  const authHeader = req.headers.authorization
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new UnauthenticatedError('Authorization invalid')
+  }
 
-const token  = authHeader.split(' ')[1]
+  const token = authHeader.split(' ')[1]
 
-try {
+  try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
-
-    //attach user to job routes
-    req.user = {userId:payload.userId, name:payload.name}
+    // attach user to job routes
+    req.user = { userId: payload.userId, name: payload.name }
     next()
-    
-} catch (error) {
+  } catch (error) {
     throw new UnauthenticatedError('invalid credentials')
-    
+  }
 }
+
+module.exports = auth
